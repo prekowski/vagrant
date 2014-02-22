42033117e1d12150756106759a336f3d2d684c6c
Pawel Rekowski <pawel.rekowski@gmail.com>
2014-02-22 18:14:52 +0000

    mysql package fix

--- bootstrap/packages/mysql.sh
+++ bootstrap/packages/mysql.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-apt-get -y install mysql-server > /dev/null  2>&1
-mysqladmin -f -u root drop test
-mysqladmin -u root password vagrant
+export DEBIAN_FRONTEND=noninteractive
+apt-get -y -f install mysql-server-5.5
+mysqladmin -f drop test
+mysqladmin -f -u root password vagrant
 mysql -f -u root --password=vagrant -D mysql <<< "DELETE FROM user WHERE password=''"
 
 echo "Init phpmyadmin"
-apt-get -y -f install phpmyadmin > /dev/null  2>&1
-ln -s /usr/share/phpmyadmin /var/www/phpmyadmin
+apt-get -y -f install phpmyadmin > /dev/null 2>&1
+ln -sf /usr/share/phpmyadmin /var/www/phpmyadmin
 service apache2 restart
